7115e3723ee2efe621186fdaa803d27eb50ce25e
Rahulzhp <akumari42872@gmail.com>
2023-07-13 15:22:24 +0530

    having problem with deploy that's why use useclient

--- src/Store/Todostore.tsx
+++ src/Store/Todostore.tsx
@@ -1,78 +1,81 @@
 "use client"
-import { useContext, useState, createContext, ReactNode } from "react";
+import { useContext, useState, createContext, ReactNode, useEffect } from "react";
 
 export type Todo = {
-    id: String;
-    title: String;
-    description: String
+    id: string;
+    title: string;
+    description: string;
     completed: boolean;
-    createdAt: Date
-}
+    createdAt: Date;
+};
+
 export type TodoContext = {
     todos: Todo[];
-    handleAddTodo: (title: String, description: String) => void
-    toggleTodo: (id: any) => void
-    handleDelte: (id: any) => void
-}
+    handleAddTodo: (title: string, description: string) => void;
+    toggleTodo: (id: string) => void;
+    handleDelete: (id: string) => void;
+};
 
-export const todoContext = createContext<TodoContext | null>(null)
-
+export const todoContext = createContext<TodoContext | null>(null);
 
 export const TodosProvider = ({ children }: { children: ReactNode }) => {
-    const [todos, setTodos] = useState<Todo[]>(() => {
-        const newTodos = localStorage.getItem("todos") || "[]";
-        return JSON.parse(newTodos) as Todo[];
-    })
-    const handleAddTodo = (title: String, description: String) => {
+    const [todos, setTodos] = useState<Todo[]>([]);
 
-        setTodos((prev: any): any => {
-            const newTodos: Todo[] = [{
-                id: Math.random().toString(),
-                title,
-                description,
-                completed: false,
-                createdAt: new Date()
-            },
-            ...prev
-            ]
-            localStorage.setItem("todos", JSON.stringify(newTodos))
-            return newTodos
-        })
-    }
+    useEffect(() => {
+        const storedTodos = localStorage.getItem("todos");
+        if (storedTodos) {
+            setTodos(JSON.parse(storedTodos));
+        }
+    }, []);
 
-    const toggleTodo = (id: String) => {
-        setTodos((prev: any): any => {
-            const newTodos: Todo[] = prev.map((el: any) => {
-                if (el.id === id) {
-                    return { ...el, completed: !el.completed }
+    const handleAddTodo = (title: string, description: string) => {
+        const newTodo: Todo = {
+            id: Math.random().toString(),
+            title,
+            description,
+            completed: false,
+            createdAt: new Date(),
+        };
+
+        setTodos((prevTodos) => {
+            const newTodos = [newTodo, ...prevTodos];
+            localStorage.setItem("todos", JSON.stringify(newTodos));
+            return newTodos;
+        });
+    };
+
+    const toggleTodo = (id: string) => {
+        setTodos((prevTodos) => {
+            const newTodos = prevTodos.map((todo) => {
+                if (todo.id === id) {
+                    return { ...todo, completed: !todo.completed };
                 }
-                return el
-            })
-            localStorage.setItem("todos", JSON.stringify(newTodos))
-            return newTodos
-        })
-    }
-    const handleDelte = (id: String) => {
-        setTodos((prev: any): any => {
-            const newTodos: Todo[] = prev.filter((el: any) => el.id !== id)
-            localStorage.setItem("todos", JSON.stringify(newTodos))
-            return newTodos
-        })
-    }
+                return todo;
+            });
+            localStorage.setItem("todos", JSON.stringify(newTodos));
+            return newTodos;
+        });
+    };
+
+    const handleDelete = (id: string) => {
+        setTodos((prevTodos) => {
+            const newTodos = prevTodos.filter((todo) => todo.id !== id);
+            localStorage.setItem("todos", JSON.stringify(newTodos));
+            return newTodos;
+        });
+    };
 
     return (
-        <todoContext.Provider value={{ todos, handleAddTodo, toggleTodo, handleDelte }}>
+        <todoContext.Provider value={{ todos, handleAddTodo, toggleTodo, handleDelete }}>
             {children}
         </todoContext.Provider>
-    )
-}
+    );
+};
 
 export function useTodos() {
-    const todosContextvalue = useContext(todoContext)
-    if (!todosContextvalue) {
-        throw new Error("useTodo used outside provider")
-
+    const todosContextValue = useContext(todoContext);
+    if (!todosContextValue) {
+        throw new Error("useTodos used outside provider");
     }
-    return todosContextvalue
-
+    return todosContextValue;
 }
